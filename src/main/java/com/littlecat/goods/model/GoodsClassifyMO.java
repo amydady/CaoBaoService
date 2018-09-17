a719c3690272654a78bdc689848e170be07d92be
amydady <215952449@qq.com>
2018-09-17 21:42:32 +0800

    NONE

--- src/main/java/com/littlecat/goods/model/GoodsClassifyMO.java
+++ src/main/java/com/littlecat/goods/model/GoodsClassifyMO.java
@@ -1,5 +1,10 @@
 package com.littlecat.goods.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
 /**
  * 商品分类
  * 
@@ -8,5 +13,91 @@ package com.littlecat.goods.model;
  */
 public class GoodsClassifyMO
 {
-	
+	private final String NO_PARENT_ID = "-1";
+
+	private String id;
+	private String name;
+	private int sortNum;
+	private String parentId = NO_PARENT_ID;
+	private String remark;
+	private String enable;
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getSortNum()
+	{
+		return sortNum;
+	}
+
+	public void setSortNum(int sortNum)
+	{
+		this.sortNum = sortNum;
+	}
+
+	public String getParentId()
+	{
+		return parentId;
+	}
+
+	public void setParentId(String parentId)
+	{
+		this.parentId = parentId;
+	}
+
+	public String getRemark()
+	{
+		return remark;
+	}
+
+	public void setRemark(String remark)
+	{
+		this.remark = remark;
+	}
+
+	public String getEnable()
+	{
+		return enable;
+	}
+
+	public void setEnable(String enable)
+	{
+		this.enable = enable;
+	}
+
+	public static class GoodsClassifyMapper implements RowMapper<GoodsClassifyMO>
+	{
+		@Override
+		public GoodsClassifyMO mapRow(ResultSet rs, int rowNum) throws SQLException
+		{
+			GoodsClassifyMO mo = new GoodsClassifyMO();
+
+			mo.setId(rs.getString("id"));
+			mo.setName(rs.getString("name"));
+			mo.setSortNum(rs.getInt("sortNum"));
+			mo.setParentId(rs.getString("parentId"));
+			mo.setRemark(rs.getString("remark"));
+			mo.setEnable(rs.getString("enable"));
+
+			return mo;
+		}
+	}
+
 }
--- src/main/java/com/littlecat/goods/model/GoodsMO.java
+++ src/main/java/com/littlecat/goods/model/GoodsMO.java
@@ -1,8 +1,79 @@
 package com.littlecat.goods.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
 import com.littlecat.cbb.common.BaseMO;
 
 public class GoodsMO extends BaseMO
 {
-	
+	private String classifyId;
+	private String supplierId;
+	private String name;
+	private String price;
+	private String enable;
+
+	public String getClassifyId()
+	{
+		return classifyId;
+	}
+
+	public void setClassifyId(String classifyId)
+	{
+		this.classifyId = classifyId;
+	}
+
+	public String getSupplierId()
+	{
+		return supplierId;
+	}
+
+	public void setSupplierId(String supplierId)
+	{
+		this.supplierId = supplierId;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(String price)
+	{
+		this.price = price;
+	}
+
+	public String getEnable()
+	{
+		return enable;
+	}
+
+	public void setEnable(String enable)
+	{
+		this.enable = enable;
+	}
+
+	public static class GoodsMapper implements RowMapper<GoodsMO>
+	{
+		@Override
+		public GoodsMO mapRow(ResultSet rs, int rowNum) throws SQLException
+		{
+			GoodsMO mo = new GoodsMO();
+
+			return mo;
+		}
+	}
+
 }
