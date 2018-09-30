12c569af574960df8b1435ab416457b755cc5ab5
amydady <215952449@qq.com>
2018-09-30 11:42:36 +0800

    NONE

---
+++ src/main/java/com/littlecat/order/rest/OrderController.java
@@ -0,0 +1,240 @@
+package com.littlecat.order.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.littlecat.cbb.common.Consts;
+import com.littlecat.cbb.exception.LittleCatException;
+import com.littlecat.cbb.query.QueryParam;
+import com.littlecat.cbb.rest.RestRsp;
+import com.littlecat.cbb.rest.RestSimpleRsp;
+import com.littlecat.order.business.OrderBusiness;
+import com.littlecat.order.model.OrderCreateReqInfo;
+import com.littlecat.order.model.OrderMO;
+
+@RestController
+@RequestMapping("/rest/littlecat/caobao/order")
+public class OrderController
+{
+	@Autowired
+	private OrderBusiness orderBusiness;
+
+	private Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+	@GetMapping(value = "/getById")
+	public RestRsp<OrderMO> getById(@RequestParam String id)
+	{
+		RestRsp<OrderMO> result = new RestRsp<OrderMO>();
+
+		try
+		{
+			OrderMO mo = orderBusiness.getOrderById(id);
+			result.getData().add(mo);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@PutMapping(value = "/setOrderState2DaiQianShou/{id}")
+	public RestSimpleRsp setOrderState2DaiQianShou(@PathVariable String id)
+	{
+		RestSimpleRsp result = new RestSimpleRsp();
+
+		try
+		{
+			orderBusiness.setOrderState2DaiQianShou(id);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@PutMapping(value = "/setOrderState2YiShouHuo/{id}")
+	public RestSimpleRsp setOrderState2YiShouHuo(@PathVariable String id)
+	{
+		RestSimpleRsp result = new RestSimpleRsp();
+
+		try
+		{
+			orderBusiness.setOrderState2YiShouHuo(id);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@PutMapping(value = "/setOrderState2TuiKuanZhong/{id}")
+	public RestSimpleRsp setOrderState2TuiKuanZhong(@PathVariable String id)
+	{
+		RestSimpleRsp result = new RestSimpleRsp();
+
+		try
+		{
+			orderBusiness.setOrderState2TuiKuanZhong(id);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@PutMapping(value = "/setOrderState2YiTuiKuan/{id}")
+	public RestSimpleRsp setOrderState2YiTuiKuan(@PathVariable String id)
+	{
+		RestSimpleRsp result = new RestSimpleRsp();
+
+		try
+		{
+			orderBusiness.setOrderState2YiTuiKuan(id);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@PostMapping(value = "/create")
+	public RestRsp<String> create(@RequestBody OrderCreateReqInfo orderCreateReqInfo)
+	{
+		RestRsp<String> result = new RestRsp<String>();
+
+		try
+		{
+			result.getData().add(orderBusiness.addOrder(orderCreateReqInfo.getOrderMO(), orderCreateReqInfo.getOrderDetailMOs()));
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@PostMapping(value = "/getList")
+	public RestRsp<OrderMO> getList(@RequestBody QueryParam queryParam)
+	{
+		RestRsp<OrderMO> result = new RestRsp<OrderMO>();
+
+		try
+		{
+			List<OrderMO> mos = new ArrayList<OrderMO>();
+			int totalNum = orderBusiness.getOrderList(queryParam, mos);
+			result.setTotalNum(totalNum);
+			result.getData().addAll(mos);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+
+	@DeleteMapping(value = "/delete/{id}")
+	public RestSimpleRsp delete(@PathVariable String id)
+	{
+		RestSimpleRsp result = new RestSimpleRsp();
+
+		try
+		{
+			orderBusiness.deleteOrderById(id);
+		}
+		catch (LittleCatException e)
+		{
+			result.setCode(e.getErrorCode());
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+		catch (Exception e)
+		{
+			result.setCode(Consts.ERROR_CODE_UNKNOW);
+			result.setMessage(e.getMessage());
+			logger.error(e.getMessage(), e);
+		}
+
+		return result;
+	}
+}
