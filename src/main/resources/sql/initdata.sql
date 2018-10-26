fe21ce3e3db531e107f37fdfdf3799555aa289af
amydady <215952449@qq.com>
2018-10-26 09:12:22 +0800

    NOONE

--- src/main/resources/sql/initdata.sql
+++ src/main/resources/sql/initdata.sql
@@ -10,10 +10,26 @@ insert into t_sys_param(name,value) values('clear_reslock_process_cyc','60');
 #系统菜单==========================================================
 
 insert into t_sys_menu(id,name,pid,sortNum) values('1','供应商管理','-1','1');
-insert into t_sys_menu(id,name,targeTurl,pid,sortNum,,isDefault) values('101','供应商列表','../Supplier/SupplierList.html','1','1','Y');
+insert into t_sys_menu(id,name,targeTurl,pid,sortNum,isDefault) values('101','供应商列表','../Supplier/SupplierList.html','1','1','Y');
 
-insert into t_sys_menu(id,name,pid,sortNum,isDefault) values('2','商品管理','-1','1','Y');
+insert into t_sys_menu(id,name,pid,sortNum,isDefault) values('2','商品管理','-1','2','Y');
 insert into t_sys_menu(id,name,targeTurl,pid,sortNum,isDefault) values('201','商品列表','../Goods/GoodsList.html','2','1','Y');
+
+insert into t_sys_menu(id,name,pid,sortNum) values('3','物流管理','-1','3');
+insert into t_sys_menu(id,name,targeTurl,pid,sortNum,isDefault) values('301','配送范围管理','../Delivery/DeliveryAreaMgr.html','3','1','Y');
+insert into t_sys_menu(id,name,targeTurl,pid,sortNum) values('302','快递费用管理','../Delivery/DeliveryFeeRuleMgr.html','3','2');
+
+insert into t_sys_menu(id,name,pid,sortNum) values('4','佣金管理','-1','4');
+insert into t_sys_menu(id,name,targeTurl,pid,sortNum,isDefault) values('401','佣金类目管理','../commission/CommissionClassifyList.html','4','1','Y');
+insert into t_sys_menu(id,name,targeTurl,pid,sortNum) values('402','商品佣金设置','../commission/GoodsCommissionMgr.html','4','2');
+
+
+
+insert into t_sys_menu(id,name,pid,sortNum) values('5','消费者管理','-1','5');
+insert into t_sys_menu(id,name,targeTurl,pid,sortNum,isDefault) values('501','消费者列表','../terminaluser/TerminalUserList.html','5','1','Y');
+
+insert into t_sys_menu(id,name,pid,sortNum) values('99','系统管理','-1','99');
+
 
 
 insert into t_sys_sysoperator(id,username,password,name,wxCode,email,mobile)
